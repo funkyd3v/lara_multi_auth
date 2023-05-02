03466b4904e775e12fdc9cd7e3659197376c768a
funkyd3v <sdiu.cse215@gmail.com>
2023-05-02 23:42:41 +0600

    admin profile edit functionality added!

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -8,10 +8,12 @@
                 <div class="card-body">
                     <div class="d-flex align-items-center justify-content-between mb-2">
                         <div>
-                            <img class="wd-100 rounded-circle" src="{{ !empty($profileData->photo) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg') }}" alt="profile">
+                            <img class="wd-100 rounded-circle"
+                                src="{{ !empty($profileData->photo) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg') }}"
+                                alt="profile">
                             <span class="h5 ms-3">Amiah Burton</span>
                         </div>
-        
+
                     </div>
                     <p>Hi! I'm Amiah the Senior UI Designer at NobleUI. We hope you enjoy the design and quality of
                         Social.</p>
@@ -50,65 +52,44 @@
         <div class="col-md-8 col-xl-8 middle-wrapper">
             <div class="row">
                 <div class="col-md-12 grid-margin">
-                    <div class="card rounded">
-                        <div class="card-header">
-                            <div class="d-flex align-items-center justify-content-between">
-                                <div class="d-flex align-items-center">
-                                    <img class="img-xs rounded-circle" src="https://via.placeholder.com/37x37" alt="">
-                                    <div class="ms-2">
-                                        <p>Mike Popescu</p>
-                                        <p class="tx-11 text-muted">1 min ago</p>
-                                    </div>
-                                </div>
-                                <div class="dropdown">
-                                    <a type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown"
-                                        aria-haspopup="true" aria-expanded="false">
-                                        <i class="icon-lg pb-3px" data-feather="more-horizontal"></i>
-                                    </a>
-                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
-                                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i
-                                                data-feather="meh" class="icon-sm me-2"></i> <span
-                                                class="">Unfollow</span></a>
-                                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i
-                                                data-feather="corner-right-up" class="icon-sm me-2"></i> <span
-                                                class="">Go to post</span></a>
-                                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i
-                                                data-feather="share-2" class="icon-sm me-2"></i> <span
-                                                class="">Share</span></a>
-                                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i
-                                                data-feather="copy" class="icon-sm me-2"></i> <span class="">Copy
-                                                link</span></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                    <div class="card">
                         <div class="card-body">
-                            <p class="mb-3 tx-14">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
-                                minima delectus nemo unde quae recusandae assumenda.</p>
-                            <img class="img-fluid" src="https://via.placeholder.com/689x430" alt="">
-                        </div>
-                        <div class="card-footer">
-                            <div class="d-flex post-actions">
-                                <a href="javascript:;" class="d-flex align-items-center text-muted me-4">
-                                    <i class="icon-md" data-feather="heart"></i>
-                                    <p class="d-none d-md-block ms-2">Like</p>
-                                </a>
-                                <a href="javascript:;" class="d-flex align-items-center text-muted me-4">
-                                    <i class="icon-md" data-feather="message-square"></i>
-                                    <p class="d-none d-md-block ms-2">Comment</p>
-                                </a>
-                                <a href="javascript:;" class="d-flex align-items-center text-muted">
-                                    <i class="icon-md" data-feather="share"></i>
-                                    <p class="d-none d-md-block ms-2">Share</p>
-                                </a>
-                            </div>
+
+                            <h6 class="card-title">Basic Form</h6>
+
+                            <form class="forms-sample">
+                                <div class="mb-3">
+                                    <label for="exampleInputUsername1" class="form-label">Username</label>
+                                    <input type="text" class="form-control" id="exampleInputUsername1"
+                                        autocomplete="off" placeholder="Username">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1"
+                                        placeholder="Email">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                                    <input type="password" class="form-control" id="exampleInputPassword1"
+                                        autocomplete="off" placeholder="Password">
+                                </div>
+                                <div class="form-check mb-3">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                    <label class="form-check-label" for="exampleCheck1">
+                                        Remember me
+                                    </label>
+                                </div>
+                                <button type="submit" class="btn btn-primary me-2">Submit</button>
+                                <button class="btn btn-secondary">Cancel</button>
+                            </form>
+
                         </div>
                     </div>
                 </div>
             </div>
         </div>
         <!-- middle wrapper end -->
-        </div>
+    </div>
 
 </div>
 @endsection
